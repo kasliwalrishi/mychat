a5a5e36e1823b55b4af2cc3482adb472e1eb5431
Rishi Kasliwal <rishikasliwal5@gmail.com>
2025-02-28 19:43:55 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/mychat/",  // <-- Add this line (replace "mychat" with your repo name)
   plugins: [react()],
 })
